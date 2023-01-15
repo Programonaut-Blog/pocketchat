dbc8a794e19dc0ab5e271ec15167992bf12d8105
programonaut <dev@maximilian-kuerschner.de>
2023-01-15 11:49:47 +0100

    feat: types

--- src/types/pocketbase-types.ts
+++ src/types/pocketbase-types.ts
@@ -3,6 +3,9 @@
 */
 
 export enum Collections {
+	Chats = "chats",
+	ChatsUsers = "chats_users",
+	Messages = "messages",
 	Users = "users",
 }
 
@@ -29,14 +32,34 @@ export type AuthSystemFields = {
 
 // Record types for each collection
 
+export type ChatsRecord = {
+	chat_name?: string
+}
+
+export type ChatsUsersRecord = {
+	chat_id?: RecordIdString
+	user_id?: RecordIdString
+}
+
+export type MessagesRecord = {
+	chat_id?: RecordIdString
+	message?: string
+}
+
 export type UsersRecord = {
 	name?: string
 	avatar?: string
 }
 
 // Response types include system fields and match responses from the PocketBase API
+export type ChatsResponse = ChatsRecord & BaseSystemFields
+export type ChatsUsersResponse = ChatsUsersRecord & BaseSystemFields
+export type MessagesResponse = MessagesRecord & BaseSystemFields
 export type UsersResponse = UsersRecord & AuthSystemFields
 
 export type CollectionRecords = {
+	chats: ChatsRecord
+	chats_users: ChatsUsersRecord
+	messages: MessagesRecord
 	users: UsersRecord
 }
